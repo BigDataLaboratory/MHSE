116386e6d4d723d319344201c501da9ea252259a
danielepasquini <Aj01.Pemo89>
2020-06-17 17:02:10 +0200

    Added multithread boolean version of minhash algorithm (v0.1.0)

--- src/main/java/it/bigdatalab/algorithm/MultithreadBMinHash.java
+++ src/main/java/it/bigdatalab/algorithm/MultithreadBMinHash.java
@@ -1,16 +1,230 @@
 package it.bigdatalab.algorithm;
 
 import it.bigdatalab.model.GraphMeasure;
+import it.bigdatalab.utils.PropertiesManager;
+import it.unimi.dsi.fastutil.ints.*;
+import it.unimi.dsi.webgraph.ImmutableGraph;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MultithreadBMinHash extends MinHash {
 
-    public MultithreadBMinHash() throws IOException, DirectionNotSetException, SeedsException {
+    private static final int MASK = 6; // 2^6
+    private static final int REMAINDER = 58;
+    private static final long BIT = 1;
+
+    private Int2LongSortedMap mTotalCollisions;
+    private int mNumberOfThreads;
+    private final Object mLock = new Object();
+
+    /**
+     * Creates a new BooleanMinHash instance with default values
+     */
+    public MultithreadBMinHash() throws DirectionNotSetException, SeedsException, IOException {
+        super();
+        int suggestedNumberOfThreads = Integer.parseInt(PropertiesManager.getProperty("minhash.suggestedNumberOfThreads"));
+        logger.info("Number of threads selected {}", suggestedNumberOfThreads);
+
+        logger.info("# nodes {}, # edges {}", mGraph.numNodes(), mGraph.numArcs());
+        this.mNumberOfThreads = getNumberOfMaxThreads(suggestedNumberOfThreads);
+        mTotalCollisions = new Int2LongLinkedOpenHashMap();
     }
 
-    @Override
+
     public GraphMeasure runAlgorithm() {
-        return null;
+        logger.debug("Number of threads to be used {}", mNumberOfThreads);
+        ExecutorService executor = Executors.newFixedThreadPool(mNumberOfThreads); //creating a pool of threads
+        List<Callable<Object>> todo = new ArrayList<>(this.numSeeds);
+
+        for(int i = 0; i < this.numSeeds; i++) {
+            todo.add(Executors.callable(new IterationThread(mGraph.copy(), i)));
+        }
+        try {
+            executor.invokeAll(todo);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        executor.shutdown();
+
+        hopTable = hopTable();
+        logger.debug("Hop table derived from collision table: {}", hopTable);
+
+        GraphMeasure graphMeasure = new GraphMeasure(hopTable);
+        graphMeasure.setNumNodes(mGraph.numNodes());
+        graphMeasure.setNumArcs(mGraph.numArcs());
+        return graphMeasure;
     }
+
+    /**
+     * @return hopTable computed for reachable pairs within h hops [(CountAllCum[h]*n) / s]
+     */
+    private Int2DoubleSortedMap hopTable() {
+        Int2DoubleSortedMap hopTable = new Int2DoubleLinkedOpenHashMap();
+        mTotalCollisions.forEach((key, value) -> {
+            if(key != 0 && value != mTotalCollisions.getOrDefault(key-1,0))  {
+                Double r = ((double) (value * mGraph.numNodes()) / this.numSeeds);
+                hopTable.put(key, r);
+            }
+        });
+        hopTable.put(0, mGraph.numNodes());
+        return hopTable;
+    }
+
+
+    /**
+     * Number of max threads to use for the computation
+     * @param suggestedNumberOfThreads if not equal to zero return the number of threads
+     *                                 passed as parameter, else the number of max threads available
+     * @return number of threads to use for the computation
+     */
+    private final int getNumberOfMaxThreads(int suggestedNumberOfThreads) {
+        if(suggestedNumberOfThreads != 0) return suggestedNumberOfThreads;
+        return Runtime.getRuntime().availableProcessors();
+    }
+
+    class IterationThread implements Runnable {
+
+        private ImmutableGraph g;
+        private int index;
+
+        public IterationThread(ImmutableGraph g, int index) {
+            this.g = g;
+            this.index = index;
+        }
+
+        @Override
+        public void run() {
+            logger.info("Starting computation on seed {}", index);
+
+            Int2LongOpenHashMap hopCollision;
+            int counter;
+
+            // Set false as signature of all graph nodes
+            // used to computing the algorithm
+            long[] mutable = new long[lengthBitsArray(g.numNodes())];
+            long[] immutable = new long[lengthBitsArray(g.numNodes())];
+
+            // Choose a random node is equivalent to compute the minhash
+            // TODO check if the range is 0-n or 0-n+1
+            int randomNode = ThreadLocalRandom.current().nextInt(0, g.numNodes());
+            // take a long number, if we divide it to a number power of 2, quotient is in the first 6 bit, remainder
+            // in the last 58 bit. So, move the remainder to the left, and then to the right to delete the quotient.
+            // This is equal to logical and operation.
+            int remainderPositionRandomNode = ((randomNode << REMAINDER) >>> REMAINDER);
+            // quotient: randomNode >>> MASK
+            mutable[randomNode >>> MASK] |= (BIT) << remainderPositionRandomNode;
+
+            int h = 0;
+
+            // initialization of the collision counter for the hop
+            // we use a dict because we want to iterate over the nodes until
+            // the number of collisions in the actual hop
+            // is different than the previous hop
+            hopCollision = new Int2LongOpenHashMap();
+            hopCollision.put(h, 1);
+
+            while(hopCollision.get(h) != hopCollision.getOrDefault(h-1,0)) {
+                h += 1;
+                logger.debug("(seed {}) Starting computation on hop {}", index, h);
+
+                // copy all the actual nodes hash in a new structure
+                System.arraycopy(mutable, 0, immutable, 0, mutable.length);
+
+                hopCollision.put(h, 0);
+
+                long remainderPositionNode;
+                int quotientNode;
+                for(int n = 0; n < g.numNodes(); n++) {
+
+                    final int node = n;
+
+                    final int d = g.outdegree(node);
+                    final int[] successors = g.successorArray(node);
+
+                    // update the node hash iterating over all its neighbors
+                    // and computing the OR between the node signature and
+                    // the neighbor signature.
+                    // store the new signature as the current one
+                    remainderPositionNode = (node << REMAINDER) >>> REMAINDER;
+                    quotientNode = node >>> MASK;
+
+                    long value = immutable[quotientNode];
+                    long bitNeigh;
+                    long nodeMask = (1L << remainderPositionNode);
+
+                    if(((nodeMask & value) >>> remainderPositionNode) == 0) { // check if node bit is 0
+                        for (int l = 0; l < d; l++) {
+                            final int neighbour = successors[l];
+
+
+                            int quotientNeigh = neighbour >>> MASK;
+                            long remainderPositionNeigh = (neighbour << REMAINDER) >>> REMAINDER;
+
+                            bitNeigh = (((1L << remainderPositionNeigh) & immutable[quotientNeigh]) >>> remainderPositionNeigh) << remainderPositionNode;
+                            value = bitNeigh | nodeMask & immutable[quotientNode];
+                            if ((value >>> remainderPositionNode) == 1) {
+                                break;
+                            }
+                        }
+                    }
+                    mutable[quotientNode] = mutable[quotientNode] | value;
+                }
+
+                // count the collision between the node signature and the graph signature
+                counter = 0;
+                for(int c = 0; c < mutable.length; c++) {
+                    counter += Long.bitCount(mutable[c]);
+                }
+                hopCollision.put(h, counter);
+                logger.debug("(seed {}) Hop Collision {}", index, hopCollision);
+
+            }
+
+            // compute the collision table
+            synchronized (MultithreadBMinHash.this.mLock) {
+                logger.debug("(seed {}) Locking computation on collision table", index);
+                logger.debug("(seed {}) Hop collision {}", index, hopCollision);
+                long lastElement = MultithreadBMinHash.this.mTotalCollisions.get(MultithreadBMinHash.this.mTotalCollisions.size()-1);
+
+                if(hopCollision.size() <= MultithreadBMinHash.this.mTotalCollisions.size()) {
+                    for(int k = 0; k < hopCollision.size(); k++) {
+                        long sumCollisions = MultithreadBMinHash.this.mTotalCollisions.get(k) + hopCollision.get(k);
+                        MultithreadBMinHash.this.mTotalCollisions.put(k, sumCollisions);
+                    }
+                    for(int k = hopCollision.size(); k < MultithreadBMinHash.this.mTotalCollisions.size(); k++) {
+                        long sumCollisions = MultithreadBMinHash.this.mTotalCollisions.get(k) + hopCollision.get(hopCollision.size()-1);
+                        MultithreadBMinHash.this.mTotalCollisions.put(k, sumCollisions);
+                    }
+                } else {
+                    for(int k = 0; k < MultithreadBMinHash.this.mTotalCollisions.size(); k++) {
+                        long sumCollisions = MultithreadBMinHash.this.mTotalCollisions.get(k) + hopCollision.get(k);
+                        MultithreadBMinHash.this.mTotalCollisions.put(k, sumCollisions);
+                    }
+                    for(int k = MultithreadBMinHash.this.mTotalCollisions.size(); k < hopCollision.size(); k++) {
+                        long sumCollisions = lastElement + hopCollision.get(k);
+                        MultithreadBMinHash.this.mTotalCollisions.put(k, sumCollisions);
+                    }
+                }
+                logger.debug("(seed {}) Collision table {}", index, mTotalCollisions);
+                logger.debug("(seed {}) Unlocking computation on collision table", index);
+                logger.debug("Ended computation on seed {}", index);
+            }
+
+        }
+
+
+        private int lengthBitsArray(int numberOfNodes) {
+            return (int) Math.ceil(numberOfNodes/ (double) Long.SIZE);
+        }
+
+    }
+
 }
+
+
