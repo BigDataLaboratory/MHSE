75098d90c7d007e2d10dfa777bb1613062e5e9ed
danielepasquini <Aj01.Pemo89>
2020-06-17 16:03:14 +0200

    Added MultithreadBMinHash class to implement the parallel version of boolean minhash algorithm

---
+++ src/main/java/it/bigdatalab/algorithm/MultithreadBMinHash.java
@@ -0,0 +1,16 @@
+package it.bigdatalab.algorithm;
+
+import it.bigdatalab.model.GraphMeasure;
+
+import java.io.IOException;
+
+public class MultithreadBMinHash extends MinHash {
+
+    public MultithreadBMinHash() throws IOException, DirectionNotSetException, SeedsException {
+    }
+
+    @Override
+    public GraphMeasure runAlgorithm() {
+        return null;
+    }
+}
