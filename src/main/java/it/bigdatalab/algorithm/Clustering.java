2417f70c69d3322c03a512cf823a67ef4556930d
Antonio Widnows <crc.antonio.12@gmail.com>
2020-09-19 18:49:40 +0200

    Added pagerank function. (Test needed)

--- src/main/java/it/bigdatalab/algorithm/Clustering.java
+++ src/main/java/it/bigdatalab/algorithm/Clustering.java
@@ -3,6 +3,7 @@ package it.bigdatalab.algorithm;
 import edu.uci.ics.jung.algorithms.scoring.PageRank;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import it.unimi.dsi.webgraph.ImmutableGraph;
+import it.unimi.dsi.webgraph.LazyIntIterator;
 import it.unimi.dsi.webgraph.NodeIterator;
 
 
@@ -17,6 +18,7 @@ public class Clustering {
     private long [] clustersLables;
     private long [] clustersSignatures;
     private long [] clustersCentroids;
+    private Dictionary<Integer,Double> PageRank;
     private Int2ObjectOpenHashMap<long [] > clusterAssignments;
 
 
@@ -26,58 +28,84 @@ public class Clustering {
 
 
     }
-    public Int2ObjectOpenHashMap<Double> pagerank(ImmutableGraph G,float alpha , double[]  personalization, int max_iter,double tol, double[] nstart,double[] dangling ){
+    public void pagerank(ImmutableGraph G,float alpha , Dictionary personalization, int max_iter,double tol, Dictionary nstart,Dictionary dangling ){
         Int2ObjectOpenHashMap<Double> Ranking ;
         // Create a copy in (right) stochastic form
-        Dictionary W = stochastic_graph(G);
+        Dictionary <Integer,Double>  W = stochastic_graph(G);
         Integer N = G.numNodes();
-        double [] x = new double[N];
-        double [] p = new double[N];
-        double [] dangling_weights = new double[N];
+        Dictionary <Integer,Double> x = new Hashtable <Integer, Double>();
+        Dictionary <Integer,Double> p = new Hashtable <Integer, Double>();
+        Dictionary <Integer, Double> dangling_weights = new Hashtable <Integer, Double>();
         List<Integer> dangling_nodes = new ArrayList<Integer>();
         // Choose fixed starting vector if not given
-        if(nstart.length == 0){
-            for (int i = 0; i<N;i++){
-                x[i] = 1.0/N;
+        if(nstart.isEmpty()){
+            NodeIterator nodeIterator = G.nodeIterator();
+            while(nodeIterator.hasNext()){
+                int vertex = nodeIterator.nextInt();
+                x.put(vertex,1.0/N);
             }
+
         }else{
             // Normalized nstart vector
             double s = 0;
-            for (int i = 0; i<N;i++){
-                s+= nstart[i];
+            Enumeration<Integer> keys = x.keys();
+
+            while( keys.hasMoreElements() ){
+                s+= x.get(keys.nextElement());
             }
-            for (int i = 0; i<N;i++){
-                x[i] = nstart[i]/s;
+            Enumeration<Integer> keysX = x.keys();
+            while( keysX.hasMoreElements() ){
+                int key = keysX.nextElement();
+                x.put(key,x.get(key)/s);
+
             }
         }
 
-        if(personalization.length == 0){
+        if(personalization.isEmpty()){
             // Assign uniform personalization vector if not given
-            for (int i = 0; i<N;i++){
-                p[i] = 1.0/N;
+            NodeIterator nodeIterator = G.nodeIterator();
+            while(nodeIterator.hasNext()){
+                int vertex = nodeIterator.nextInt();
+                p.put(vertex,1.0/N);
             }
+
         }else{
             // Normalized personalization vector
             double s = 0;
-            for (int i = 0; i<N;i++){
-                s+= personalization[i];
+            Enumeration<Integer> keys = p.keys();
+
+            while( keys.hasMoreElements() ){
+                s+= p.get(keys.nextElement());
             }
-            for (int i = 0; i<N;i++){
-                p[i] = personalization[i]/s;
+
+            Enumeration<Integer> keysX = p.keys();
+            while( keysX.hasMoreElements() ){
+                int key = keysX.nextElement();
+                p.put(key,p.get(key)/s);
+
             }
+
+
         }
-        if(dangling.length == 0){
+        if(dangling.isEmpty()){
             // Use personalization vector if dangling vector not specified
             dangling_weights = p;
         }else{
             // Normalized personalization vector
             double s = 0;
-            for (int i = 0; i<N;i++){
-                s+= dangling[i];
+            Enumeration<Integer> keys = dangling_weights.keys();
+
+            while( keys.hasMoreElements() ){
+                s+= dangling_weights.get(keys.nextElement());
             }
-            for (int i = 0; i<N;i++){
-                dangling_weights[i] = dangling[i]/s;
+
+            Enumeration<Integer> keysX = dangling_weights.keys();
+            while( keysX.hasMoreElements() ){
+                int key = keysX.nextElement();
+                dangling_weights.put(key,dangling_weights.get(key)/s);
+
             }
+
         }
         NodeIterator nodeIterator = G.nodeIterator();
         while(nodeIterator.hasNext()){
@@ -92,27 +120,67 @@ public class Clustering {
         // power iteration: make up to max_iter iterations
 
         for (int i = 0; i<max_iter;i++){
-            double [] xlast = x;
+            Dictionary <Integer,Double> xlast = x;
+
+            Enumeration<Integer> keysX = x.keys();
+            while( keysX.hasMoreElements() ){
+                int key = keysX.nextElement();
+                x.put(key,0.0);
+
+            }
+
+            double s = 0;
+
+            for(int n=0;n< dangling_nodes.size();n++){
+                s+= xlast.get(n);
+            }
+            double danglesum = alpha * s;
+
+            Enumeration<Integer> keys = x.keys();
+
+            while( keys.hasMoreElements() ){
+                int n = keys.nextElement();
+
+                LazyIntIterator successors = G.successors(n);
+                int nbr;
+                while((nbr=successors.nextInt())!=-1){
+                    x.put(nbr,x.get(nbr)+(alpha*xlast.get(n) * W.get(n)) );
+                }
+                x.put(n,x.get(n)+ danglesum * dangling_weights.get(n) *(1.0-alpha)*p.get(n));
+
+            }
+            // Check the L1 norm
+            double err = 0;
+
+            Enumeration<Integer> k = x.keys();
+
+            while( k.hasMoreElements() ){
+                int c = keys.nextElement();
+                err += Math.abs(x.get(c)-xlast.get(c));
+            }
+            if(err<N*tol){
+                this.PageRank = x;
+            }
 
         }
-
-
+        System.out.println("Error ! Pagerank not converged");
     }
+
     // Get the transition probability for each node
     public Dictionary stochastic_graph(ImmutableGraph G){
 
-        Dictionary weights = new Hashtable();
+        Dictionary<Integer,Double> weights = new Hashtable<Integer,Double> ();
         NodeIterator nodeIterator = G.nodeIterator();
 
         while(nodeIterator.hasNext()){
             int vertex = nodeIterator.nextInt();
             Integer outDeg = G.outdegree(vertex);
             if (outDeg == 0){
-                System.out.println("zero out-degree for node: "+v);
-                weights.put(v,0.0);
+                System.out.println("zero out-degree for node: "+vertex);
+                weights.put(vertex,0.0);
             }else{
                 double archWeights = 1.0/outDeg;
-                weights.put(v,archWeights);
+                weights.put(vertex,archWeights);
             }
         }
         return(weights);
