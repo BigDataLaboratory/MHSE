9e182762dc5e53c02f039b02179b52255dcffbd5
antonio <antonio.cruciani.uniroma2@gmail.com>
2020-10-29 11:54:16 +0100

    Aggiunto il Preprocessing

--- src/main/java/it/bigdatalab/algorithm/MinHash.java
+++ src/main/java/it/bigdatalab/algorithm/MinHash.java
@@ -2,9 +2,10 @@ package it.bigdatalab.algorithm;
 
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
-import it.bigdatalab.utils.Preprocessing;
 import it.unimi.dsi.fastutil.ints.*;
+import it.unimi.dsi.logging.ProgressLogger;
 import it.unimi.dsi.webgraph.ImmutableGraph;
+import it.unimi.dsi.webgraph.NodeIterator;
 import it.unimi.dsi.webgraph.Transform;
 import it.bigdatalab.model.GraphMeasure;
 import it.bigdatalab.utils.PropertiesManager;
@@ -12,9 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
 import java.util.Random;
 
 public abstract class MinHash {
@@ -70,9 +68,34 @@ public abstract class MinHash {
             throw new DirectionNotSetException("Direction property (\"minhash.direction\") not correctly set in properties file");
         }
         if(!isolatedVertices){
-            Preprocessing mapper = new Preprocessing(mGraph);
-            mapper.run();
-            mGraph = mapper.get_mProccessedGraph();
+            logger.info("Deleting isolated nodes...");
+            ProgressLogger pl = new ProgressLogger();
+            NodeIterator nodeIterator = mGraph.nodeIterator();
+            int[] indegree = new int[mGraph.numNodes()];
+            int [] outdegree = new int[mGraph.numNodes()];
+            int [] mappedGraph = new int[mGraph.numNodes()];
+            int numNodes = mGraph.numNodes();
+            int d;
+            int s;
+            while(numNodes-- != 0) {
+                int vertex = nodeIterator.nextInt();
+                d = nodeIterator.outdegree();
+                outdegree[vertex] = d;
+                int[] neighbours = nodeIterator.successorArray();
+                for (s = d; s-- != 0; ++indegree[neighbours[s]]) {}
+            }
+            d = 0;
+            for (s = 0; s< indegree.length;s++){
+                if((indegree[s] == 0) && (outdegree[s] ==0)){
+                    mappedGraph[s] = -1;
+                }else{
+                    mappedGraph[s] = d;
+                    d+=1;
+                }
+            }
+            int oldNumNodes = mGraph.numNodes();
+            mGraph = Transform.map(mGraph,mappedGraph,pl);
+            logger.info("Deleted {} nodes ",oldNumNodes-mGraph.numNodes());
 
         }
 
