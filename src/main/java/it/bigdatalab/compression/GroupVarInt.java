091e1fc9c5d71174e5df436884d41405c133cb20
antonio <antonio.cruciani@gssi.it>
2021-01-07 21:07:17 +0100

    Compression branch

---
+++ src/main/java/it/bigdatalab/compression/GroupVarInt.java
@@ -0,0 +1,29 @@
+package it.bigdatalab.compression;
+
+public class GroupVarInt {
+
+
+    public  GroupVarInt(){
+
+    }
+
+
+    public static int log2(int x)
+    {
+        return (int) (Math.log(x) / Math.log(2));
+    }
+
+    public void EncodeList(int [] list)
+    {
+
+
+    }
+
+    public int EncodeGroup(int [] group){
+
+        int n0 = 1;
+
+        return(1);
+    }
+
+}
