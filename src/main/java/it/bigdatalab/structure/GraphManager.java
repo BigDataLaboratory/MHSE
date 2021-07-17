1f46c23a043387a54b5baf9631f8f449b11b4e04
antonio_cruciani <antonio.cruciani@gssi.it>
2021-07-17 14:29:19 +0200

    Defined GraphManager class that allows to use both: cGraph and mGraph datastructures.

--- src/main/java/it/bigdatalab/structure/GraphManager.java
+++ src/main/java/it/bigdatalab/structure/GraphManager.java
@@ -1,25 +1,51 @@
 package it.bigdatalab.structure;
 
+import it.bigdatalab.utils.Constants;
 import it.unimi.dsi.webgraph.ImmutableGraph;
 import it.unimi.dsi.webgraph.NodeIterator;
+import it.unimi.dsi.webgraph.Transform;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class GraphManager {
+    public static final Logger logger = LoggerFactory.getLogger("it.bigdatalab.structure.GraphManager");
+
     private CompressedGraph cGraph;
     private ImmutableGraph mGraph;
     private boolean  webGraph = false;
     private boolean compressedGraph = false;
     private boolean differentialCompression = true;
+    private boolean inMemory = true;
 
     private int [] nodes;
 
-    public GraphManager(boolean WG, boolean CG,String inputFilePath) throws IOException {
+    public GraphManager(boolean WG, boolean CG,String inputFilePath, boolean transpose,String direction) throws IOException {
         int i;
 
         if(WG){
             webGraph = true;
-            // load the graph in mgraph MUST BE DEFINED
+
+            ImmutableGraph graph = inMemory ?
+                Transform.transpose(Transform.transpose(ImmutableGraph.load(inputFilePath))) :
+                ImmutableGraph.load(inputFilePath);
+
+
+            if (transpose) {
+                if (direction.equals(Constants.IN_DIRECTION)) {
+                    logger.info("Transposing graph cause direction is {}", direction);
+                    graph = Transform.transpose(graph);
+                    logger.debug("Transposing graph ended");
+                }
+            } else {
+                    if (direction.equals(Constants.OUT_DIRECTION)) {
+                        logger.info("Transposing graph cause direction is {}", direction);
+                        graph = Transform.transpose(graph);
+                        logger.debug("Transposing graph ended");
+                    }
+            }
+            mGraph = graph;
             nodes = new int[mGraph.numNodes()];
             // populate the array of nodes
             NodeIterator nodeIter;
@@ -31,9 +57,11 @@ public class GraphManager {
             }
 
 
+
         }
         if(CG){
             compressedGraph = true;
+            // Must manage the transpose loading!
             String[] SplitInputFilePath = inputFilePath.split(".");
             cGraph = new CompressedGraph(inputFilePath,SplitInputFilePath[0]+"_offset.txt",true);
             nodes = cGraph.get_nodes();
