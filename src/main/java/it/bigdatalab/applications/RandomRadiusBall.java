663c1b60971089592886297942986f67fa86c99e
Antonio <antonio.cruciani@gssi.it>
2024-04-06 13:35:14 +0200

    Implemented Random Radius Ball Algorithm

--- src/main/java/it/bigdatalab/applications/RandomRadiusBall.java
+++ src/main/java/it/bigdatalab/applications/RandomRadiusBall.java
@@ -3,10 +3,15 @@ package it.bigdatalab.applications;
 import it.bigdatalab.model.GraphMeasureOpt;
 import it.bigdatalab.model.Measure;
 import it.bigdatalab.model.Parameter;
+import it.bigdatalab.utils.Constants;
 import it.unimi.dsi.fastutil.ints.IntArrayList;
 import it.unimi.dsi.webgraph.ImmutableGraph;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RandomRadiusBall {
 
@@ -30,15 +35,56 @@ public class RandomRadiusBall {
 
 
     public Measure runRBB(){
+        long startTime = System.currentTimeMillis();
+        long totalTime,logTime,hopStartTime;
+        long lastLogTime = startTime;
+        int i,n,tau,h;
+        n = mGraph.numNodes();
+        double[] dist = new double[n];
         float [] centrality = new float[mGraph.numNodes()];
         double r;
-        int i,n;
-        n = mGraph.numNodes();
 
-        for (i = 0; i <n; i++){
+
+        for (i = 0; i <n; i++) {
             r = Math.random();
+            tau = (int) Math.floor(1 /  (r / t));
+            //BFS of depth tau from i
+            Arrays.fill(dist, -1);
+            Queue<Integer> ball = new LinkedList<>();
+            ball.add(i);
+            dist[i] = 0;
+            h = 0;
+            while (ball.size() != 0 && h < tau) {
+                hopStartTime = System.currentTimeMillis();
+                int w = ball.remove();
+                final int d = mGraph.outdegree(w);
+                final int[] successors = mGraph.successorArray(w);
+                for (int l = 0; l < d; l++) {
+                    if (dist[successors[l]] == -1) {
+                        dist[successors[l]] = dist[w] + 1;
+                        centrality[successors[l]] += 1;
+                        ball.add(successors[l]);
+                    }
+                }
 
+                logTime = System.currentTimeMillis();
+                if (logTime - lastLogTime >= Constants.LOG_INTERVAL) {
+                    logger.info("# nodes analyzed {} / {} for hop {} [elapsed {}, node/s {}]",
+                            n, mGraph.numNodes(),
+                            h,
+                            (logTime - hopStartTime) / (double) 1000,
+                            ((n + 1) / ((logTime - hopStartTime) / (double) 1000)));
+                    lastLogTime = logTime;
+                }
+                h += 1;
+
+            }
         }
+
+        for (i = 0; i <n; i++) {
+            centrality[i] = centrality[i] /t;
+        }
+
 
 
 
