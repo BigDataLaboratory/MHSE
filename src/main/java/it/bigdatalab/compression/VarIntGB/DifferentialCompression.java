dac3218affdddb7fdcf228dc2a99e51f4cd04ece
nia <16371386+niaBaldoni@users.noreply.github.com>
2025-06-09 13:23:33 +0200

    re-imported GroupVarInt and DifferentialCompression from compressionIntegration branch

--- src/main/java/it/bigdatalab/compression/VarIntGB/DifferentialCompression.java
+++ src/main/java/it/bigdatalab/compression/VarIntGB/DifferentialCompression.java
@@ -1,10 +1,4 @@
-package it.bigdatalab.compression;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sound.midi.SysexMessage;
-import java.util.Arrays;
-
+package it.bigdatalab.compression.VarIntGB;
 
 /**
  * Implementation of gap compression algorithm
@@ -15,17 +9,15 @@ import java.util.Arrays;
  * @author Daniele Pasquini
  * @author Paola Vocca
  */
-public class DifferentialCompression {
-    public static final Logger logger = LoggerFactory.getLogger("it.bigdatalab.compression.DifferentialCompression");
 
+public class DifferentialCompression {
 
     /**
      * Creates a new Differential Compression instance
      */
-    public DifferentialCompression(){
+    public DifferentialCompression() {
 
     }
-
 
     /**
      * Compute bitwise addition between two numbers
@@ -33,15 +25,14 @@ public class DifferentialCompression {
      * @param j
      * @return the sum i+j
      */
-    private int bitwiseAdd(int i, int j)
-    {
+    private int bitwiseAdd(int i, int j) {
         int uncommonBitsFromBoth = i ^ j;
         int commonBitsFromBoth   = i & j;
 
         if (commonBitsFromBoth == 0) {
-
             return uncommonBitsFromBoth;
         }
+
         return bitwiseAdd (
                 uncommonBitsFromBoth,
                 commonBitsFromBoth << 1
@@ -54,15 +45,12 @@ public class DifferentialCompression {
      * @param y
      * @return the difference x-y
      */
-    private int bitwiseSubtraction(int x,int y){
-
-        while (y != 0){
-
+    private int bitwiseSubtraction(int x, int y) {
+        while (y != 0) {
             int borrow = (~x) & y;
             x = x ^ y;
             y = borrow << 1;
         }
-
         return x;
     }
 
@@ -71,18 +59,19 @@ public class DifferentialCompression {
      * @param sequence Array of a sequence of integers (MUST BE SORTED IN INCREASING WAY)
      * @return encoded sequence
      */
-    public int[] encodeSortedSequence(int [] sequence ){
+    public int[] encodeSortedSequence(int[] sequence) {
         int[] encoded = new int[sequence.length];
         int j;
-        if(sequence.length<3){
-            return(sequence);
-        }else{
+
+        if (sequence.length < 3) {
+            return sequence;
+        } else {
             encoded[0] = sequence[0];
             encoded[1] = sequence[1];
-            for(j = 2;j<sequence.length;j++){
+            for (j = 2; j < sequence.length; j++) {
                 encoded[j] = bitwiseSubtraction(sequence[j],sequence[j-1]);
             }
-            return(encoded);
+            return encoded;
         }
     }
 
@@ -91,39 +80,35 @@ public class DifferentialCompression {
      * @param encoded Array of a encoded integers
      * @return decoded sequence
      */
-    public int[] decodeSequence(int []encoded){
-        int [] decoded = new int[encoded.length];
+    public int[] decodeSequence(int[] encoded) {
+        int[] decoded = new int[encoded.length];
         int j;
-        if(encoded.length<3){
-            return (encoded);
-        }else{
+
+        if (encoded.length < 3) {
+            return encoded;
+        } else {
             decoded[0] = encoded[0];
             decoded[1] = encoded[1];
-            for(j = 2;j<encoded.length;j++){
+            for (j = 2; j < encoded.length; j++) {
                 decoded[j] = bitwiseAdd(encoded[j],decoded[j-1]);
             }
             return (decoded);
         }
     }
 
-
     /**
      * Compute the encoding of an adjacency list
      * @param matrix Adjacency list of a graph
      * @return encoded adjacency list
      */
-    public int [][] ecnodeAdjList(int [][] matrix){
-        int [][] encoded;
-        int [] edgelist;
-        int i,j;
-        logger.info("Starting encoding the Adjacency List " );
-        encoded = new int[matrix.length][];
-        for (i = 0;i<matrix.length;i++){
-            edgelist = new int[matrix[i].length];
-            for(j=0;j<matrix[i].length;j++){
-                edgelist[j] = matrix[i][j];
+    public int[][] encodeAdjList(int[][] matrix) {
+        int[][] encoded = new int[matrix.length][];
 
-            encoded[i] = encodeSortedSequence(edgelist);
+        for (int i = 0; i < matrix.length; i++) {
+            int[] edgelist = new int[matrix[i].length];
+            for (int j = 0; j < matrix[i].length; j++) {
+                edgelist[j] = matrix[i][j];
+                encoded[i] = encodeSortedSequence(edgelist);
             }
         }
         return (encoded);
@@ -134,22 +119,22 @@ public class DifferentialCompression {
      * @param matrix Encoded adjacency list of a graph
      * @return decoded adjacency list
      */
-    public int [][] decodeAdjList(int [][] matrix){
-        int [][] decoded;
-        int [] edgelist;
+    public int[][] decodeAdjList(int[][] matrix) {
+        int[][] decoded;
+        int[] edgelist;
         int i,j;
-        logger.info("Starting encoding the Adjacency List " );
-        decoded = new int[matrix.length][];
-        for (i = 0;i<matrix.length;i++){
-            edgelist = new int[matrix[i].length];
-            for(j=0;j<matrix[i].length;j++){
-                edgelist[j] = matrix[i][j];
 
+        decoded = new int[matrix.length][];
+
+        for (i = 0; i < matrix.length; i++) {
+            edgelist = new int[matrix[i].length];
+
+            for (j = 0; j < matrix[i].length; j++) {
+                edgelist[j] = matrix[i][j];
                 decoded[i] = decodeSequence(edgelist);
             }
         }
         return (decoded);
     }
-
 
 }
--- src/main/java/it/bigdatalab/compression/VarIntGB/VarIntGB.java
+++ src/main/java/it/bigdatalab/compression/VarIntGB/VarIntGB.java
@@ -1,17 +1,11 @@
-package it.bigdatalab.compression;
-import com.google.common.io.Closer;
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+package it.bigdatalab.compression.VarIntGB;
 
-import javax.sound.midi.SysexMessage;
-import java.awt.*;
-import java.io.*;
-import java.lang.Math.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
+import org.apache.commons.io.FileUtils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Implementation of Group VarInt compression algorithm
@@ -22,33 +16,26 @@ import java.util.Arrays;
  * @author Daniele Pasquini
  * @author Paola Vocca
  */
-public class GroupVarInt {
-    public static final Logger logger = LoggerFactory.getLogger("it.bigdatalab.compression.GroupVarint");
 
-
+public class VarIntGB {
     private int[][] offset;
-
     private byte[] compressedOffset;
     private byte[][] compressedAdjList;
     private byte[] compressedAdjListFlat;
-    private DifferentialCompression GapCompressor;
 
     /**
      * Creates a new Group VarInt Compression instance
      */
-    public  GroupVarInt(){
-
+    public VarIntGB() {
 
     }
-
 
     /**
      * Computes the length the log base 2 of an integer
      * @param x integer
      * @return log base 2
      */
-    public static float log2(int x)
-    {
+    public static float log2(int x) {
         return (float) (Math.log(x) / Math.log(2));
     }
 
@@ -57,11 +44,11 @@ public class GroupVarInt {
      * @param number integer
      * @return length of the binary representation
      */
-    public int get_bytes_number(int number){
-        if(number != 0) {
+    public int get_bytes_number(int number) {
+        if (number != 0) {
             return (int) (Math.floor(log2(number)) / 8) +1;
         }
-        return(1);
+        return (1);
     }
 
     /**
@@ -71,17 +58,17 @@ public class GroupVarInt {
      */
     public int convertByteArrayToInt(byte[] data) {
         if (data == null ) return 0x0;
-        if(data.length == 1){
+        if (data.length == 1){
             return ((data[0] & 0xFF) << 0 );
-        }else if(data.length == 2){
-            return   ((data[0] & 0xFF) << 8 ) |
+        } else if (data.length == 2) {
+            return ((data[0] & 0xFF) << 8 ) |
                     ((data[1] & 0xFF) << 0 );
-        }else if(data.length == 3){
+        } else if (data.length == 3) {
             return ((data[0] & 0xFF) << 16) |
                     ((data[1] & 0xFF) << 8 ) |
                     ((data[2] & 0xFF) << 0 );
         }
-        return  ((data[0] & 0xFF) << 24) |
+        return ((data[0] & 0xFF) << 24) |
                 ((data[1] & 0xFF) << 16) |
                 ((data[2] & 0xFF) << 8 ) |
                 ((data[3] & 0xFF) << 0 );
@@ -94,7 +81,7 @@ public class GroupVarInt {
      * @return byte array representation of the integer
      */
     private static byte[] intToBytes(final int data, int size) {
-        byte [] toBitArray = new byte[] {
+        byte[] toBitArray = new byte[] {
                 (byte)((data >> 24) ),
                 (byte)((data >> 16) ),
                 (byte)((data >> 8) ),
@@ -103,7 +90,7 @@ public class GroupVarInt {
         byte [] converted = new byte[size];
         int i = 0;
         int j = toBitArray.length-1;
-        while(i <size){
+        while (i < size) {
             converted[i] = toBitArray[j];
             i++;
             j--;
@@ -117,7 +104,7 @@ public class GroupVarInt {
      * @param group array of 4 integers
      * @return encoded group
      */
-    public byte[] EncodeGroup(int [] group){
+    public byte[] EncodeGroup(int[] group){
         /*
             Array for the number of bytes of each element of the group
             If the group has lenght strictly less than 4 the coordinates of number_prefix that are not in group are 0
@@ -127,37 +114,35 @@ public class GroupVarInt {
         // esso conta anche gli 8 bits del prefisso
         int byte_array_lenght = 1;
         //int [] partial_encoding = new int [group.length];
-        int i,j,l;
+        int i, j, l;
         int k;
         int resto;
         int quotient;
 
-        for (i = 0; i<group.length; i++){
-                byte_array_lenght+=get_bytes_number(group[i]);
+        for (i = 0; i < group.length; i++) {
+            byte_array_lenght += get_bytes_number(group[i]);
         }
 
-        byte [] partial_encoding = new byte [byte_array_lenght];
+        byte[] partial_encoding = new byte [byte_array_lenght];
 
         //int shift = 8;
         // Index inizia da 1 perché l'indice 0 dell'array di bytes è riservato al prefisso
         int index = 1;
-        for (i = 0; i<4; i++){
-            if(i<group.length){
+        for (i = 0; i < 4; i++) {
+            if (i < group.length) {
                 numbers_prefix[i] = get_bytes_number(group[i]);
 
-                byte [] encoded = new byte[numbers_prefix[i]];
+                byte[] encoded = new byte[numbers_prefix[i]];
 
-                byte [] converted_resto;
+                byte[] converted_resto;
                 k = group[i];
-                j = numbers_prefix[i]-1;
-                while(k>0){
-
-
+                j = numbers_prefix[i] - 1;
+                while (k > 0) {
                     resto = k & 0xFF; // k % 256
-                    quotient = k>>8; // k // 256
+                    quotient = k >> 8; // k // 256
 
                     converted_resto = intToBytes(resto, get_bytes_number(resto));
-                    for (l = 0; l<converted_resto.length; l++){
+                    for (l = 0; l < converted_resto.length; l++){
                         encoded[j] = converted_resto[l];
                         j--;
                     }
@@ -165,11 +150,11 @@ public class GroupVarInt {
                     k = quotient;
                 }
 
-                for(l = 0; l<encoded.length;l++){
+                for (l = 0; l < encoded.length; l++) {
                     partial_encoding[index] = encoded[l];
-                    index+=1;
+                    index += 1;
                 }
-            }else{
+            } else {
                 numbers_prefix[i] = 1;
             }
         }
@@ -186,19 +171,19 @@ public class GroupVarInt {
      * @param destination Byte array
      * @return extended byte array
      */
-    private byte[] byteArrayExtend(byte[] source,byte[] destination){
-            byte[] copied = new byte[destination.length+source.length];
-            int i,j;
+    private byte[] byteArrayExtend(byte[] source, byte[] destination){
+        byte[] copied = new byte[destination.length + source.length];
+        int i, j;
 
-            for(i =0; i<destination.length;i++){
-                copied[i] = destination[i];
-            }
-            j = 0;
-            for (i = destination.length; i<destination.length+source.length; i++){
-                copied[i] = source[j];
-                j++;
-            }
-            return(copied);
+        for (i = 0; i < destination.length; i++){
+            copied[i] = destination[i];
+        }
+        j = 0;
+        for (i = destination.length; i < destination.length + source.length; i++){
+            copied[i] = source[j];
+            j++;
+        }
+        return(copied);
     }
 
     /**
@@ -207,40 +192,39 @@ public class GroupVarInt {
      * @param destination Int array
      * @return extended Int array
      */
-    private int[] intArrayExtend(int[] source,int[] destination){
-        int[] copied = new int[destination.length+source.length];
-        int i,j;
+    private int[] intArrayExtend(int[] source, int[] destination) {
+        int[] copied = new int[destination.length + source.length];
+        int i, j;
 
-        for(i =0; i<destination.length;i++){
+        for (i = 0; i < destination.length; i++) {
             copied[i] = destination[i];
         }
         j = 0;
-        for (i = destination.length; i<destination.length+source.length; i++){
+        for (i = destination.length; i < destination.length + source.length; i++) {
             copied[i] = source[j];
             j++;
         }
         return(copied);
     }
 
-
     /**
      * Compute the encoding of an array of integers
      * @param sequence array of integers
      * @return encoded byte array
      */
-    public byte[] listEncoding(int [] sequence){
-        int [] groupedSequence = new int [4];
+    public byte[] listEncoding(int[] sequence) {
+        int[] groupedSequence = new int [4];
         byte[] tmpArray = new byte[0];
         byte[] partialEncoding;
 
-        int i,k,c;
+        int i, k, c;
         k = 0;
         c = 0;
-        for(i = 0; i<sequence.length; i++){
+
+        for (i = 0; i < sequence.length; i++){
             groupedSequence[k] = sequence[i];
             k++;
-            if((k & 3) == 0) {
-
+            if ((k & 3) == 0) {
                 partialEncoding = EncodeGroup(groupedSequence);
                 tmpArray = byteArrayExtend(partialEncoding, tmpArray);
                 k = 0;
@@ -248,38 +232,38 @@ public class GroupVarInt {
             }
 
         }
-        if(c< sequence.length){
+
+        if (c < sequence.length) {
             int[] remainingEncoding = new int[k];
-            for (int y = 0; y<k;y++){
+
+            for (int y = 0; y < k; y++){
                 remainingEncoding[y] = groupedSequence[y];
             }
+
             partialEncoding = EncodeGroup(remainingEncoding);
             tmpArray = byteArrayExtend(partialEncoding,tmpArray);
-
-
         }
         return(tmpArray);
     }
 
-
     /**
-      * Compute the decoded int array
-      * @param encoded byte array
+     * Compute the decoded int array
+     * @param encoded byte array
      * @return Int array of the decoded sequence
      */
-    public int[] dec(byte encoded []){
+    public int[] dec(byte[] encoded) {
         int decodedBytes;
-        int k,c;
+        int k, c;
         int index;
-        int s1,s2,s3,s4;
+        int s1, s2, s3, s4;
         int prefix_sum;
-        int [] prefixes;
-        int [] final_decoding = new int[0];
-        byte [] partial_decoding = new byte[0];
-        int [] decoded = new int[0];
+        int[] prefixes;
+        int[] final_decoding = new int[0];
+        byte[] partial_decoding = new byte[0];
+        int[] decoded = new int[0];
 
         decodedBytes = 0;
-        while(decodedBytes<encoded.length){
+        while (decodedBytes < encoded.length) {
             // Reading prefixes
             s1 = (0xC0 & encoded[decodedBytes]) >>> 6;
             s2 = (0x30 & encoded[decodedBytes]) >>> 4;
@@ -291,93 +275,96 @@ public class GroupVarInt {
             prefixes = new int[]{s1 + 1, s2 + 1, s3 + 1, s4 + 1};
 
 
-            if(decodedBytes +prefix_sum < encoded.length){
+            if (decodedBytes + prefix_sum < encoded.length) {
 
-                decodedBytes+=1; // excluding prefix
+                decodedBytes += 1; // excluding prefix
                 decoded = new int[4];
-                for (k = 0;k<decoded.length;k++){
+                for (k = 0; k < decoded.length; k++) {
                     partial_decoding = new byte[prefixes[k]];
-                    for (c = 0;c< partial_decoding.length;c++){
+                    for (c = 0; c < partial_decoding.length; c++) {
                         partial_decoding[c] = encoded[decodedBytes];
                         decodedBytes+=1;
                     }
                     decoded[k] = convertByteArrayToInt(partial_decoding);
                 }
 
-            }else{
+            } else {
 
-                decodedBytes +=1; // excluding prefix
+                decodedBytes += 1; // excluding prefix
                 index = 0;
-                if(decodedBytes+prefix_sum -encoded.length == 0){
+
+                if (decodedBytes+prefix_sum - encoded.length == 0) {
                     index = 4;
-                }else if(decodedBytes+prefix_sum -encoded.length == 1){
+                } else if (decodedBytes+prefix_sum - encoded.length == 1) {
                     index =3;
-                }else if(decodedBytes+prefix_sum -encoded.length == 2){
+                } else if (decodedBytes+prefix_sum - encoded.length == 2) {
                     index = 2;
-                }else if(decodedBytes+prefix_sum -encoded.length == 3){
+                } else if (decodedBytes+prefix_sum - encoded.length == 3) {
                     index = 1;
                 }
+
                 decoded = new int[index];
-                for (k = 0;k<index;k++){
+
+                for (k = 0; k < index; k++) {
                     partial_decoding = new byte[prefixes[k]];
-                    for(c = 0; c< partial_decoding.length;c++){
+                    for (c = 0; c < partial_decoding.length; c++) {
                         partial_decoding[c] = encoded[decodedBytes];
-                        decodedBytes+=1;
+                        decodedBytes += 1;
                     }
                     decoded[k] = convertByteArrayToInt(partial_decoding);
                 }
             }
-
             final_decoding = intArrayExtend(decoded, final_decoding);
         }
-    return (final_decoding);
 
+        return (final_decoding);
     }
-
 
     /**
      * Return the offset 2D-Array
      * @return offset
      */
-    public int [][] getOffset(){
+    public int[][] getOffset() {
         return (offset);
     }
-
 
     /**
      * Compute the encoding of an adjacency list
      * @param matrix adjacency list
-     * @param d_compression not implemented
      * @return encoded ajacency list
      */
-    public byte [] encodeAdjListFlat(int [][] matrix,boolean d_compression){
-        int node,edge,bytes,k;
-        byte [] encodedFlat;
-        byte [][] encoded;
-        byte [] edgeListEnc;
-        int [] edgeListToEnc;
-        int [][] off;
-        logger.info("Encoding the Adjacency list using VarInt GB");
+    public byte[] encodeAdjListFlat(int[][] matrix) {
+        int node, edge, bytes, k;
+        byte[] encodedFlat;
+        byte[][] encoded;
+        byte[] edgeListEnc;
+        int[] edgeListToEnc;
+        int[][] off;
+
         encoded = new byte[matrix.length][];
         off = new int[matrix.length][2];
         bytes = 0;
-        for(node = 0;node<matrix.length;node++){
+        for (node = 0; node < matrix.length; node++) {
             edgeListToEnc = new int[matrix[node].length];
-            for(edge = 0;edge< matrix[node].length;edge++){
+
+            for (edge = 0; edge < matrix[node].length; edge++) {
                 edgeListToEnc[edge] = matrix[node][edge];
             }
+
             edgeListEnc = listEncoding(edgeListToEnc);
-            bytes+=edgeListEnc.length;
+            bytes += edgeListEnc.length;
             encoded[node] = edgeListEnc;
             off[node][0] = matrix[node][0];
             off[node][1] = bytes;
         }
+
         encodedFlat = new byte[bytes];
         k = 0;
-        for(node =0;node<encoded.length;node++){
-            for (edge = 0;edge<encoded[node].length;edge++){
+
+        for (node = 0; node < encoded.length; node++) {
+            for (edge = 0; edge < encoded[node].length; edge++) {
                 encodedFlat[k] = encoded[node][edge];
-                k+=1;
+                k += 1;
             }
         }
 
@@ -385,62 +372,30 @@ public class GroupVarInt {
         compressedAdjList = encoded;
         compressedAdjListFlat = encodedFlat;
         return (encodedFlat);
-  }
-
+    }
 
     /**
      * Save on the disk the encoded adjacency list and its ofsset
      * @param outPath String of the output path
-     * @param instance String of the name of the file
      */
-    public void saveEncoding(String outPath,String instance) {
-
+    public void saveEncoding(String outPath) {
         int n, m, i, k, q, j;
         m = 0;
         n = offset.length;
-        logger.info("Writing the encoded Graph and the offset file " );
-
-
         // Writing flattered encoding
+        File f = new File(outPath + ".txt");
+
         try {
-            File f = new File(outPath + instance + ".txt");
-            if (f.createNewFile()) {
-                logger.info("File {} created ", f.getName());
-
-            } else {
-                logger.error("File already exists.");
-            }
-
-        } catch (IOException e) {
-            logger.error("An error occurred.");
-            e.printStackTrace();
-        }
-        try {
-            FileUtils.writeByteArrayToFile(new File(outPath+ instance + ".txt"), compressedAdjListFlat);
-
-            logger.info("Successfully written data to the file ");
-
+            FileUtils.writeByteArrayToFile(new File(outPath + ".txt"), compressedAdjListFlat);
         } catch (IOException e) {
             e.printStackTrace();
         }
         // Writing offset file
 
-        try {
-            File off = new File(outPath + instance + "_offset.txt");
-            if (off.createNewFile()) {
-                logger.info("File {} created ", off.getName());
-
-            } else {
-                logger.error("File already exists.");
-            }
-        } catch (IOException e) {
-            logger.error("An error occurred.");
-            e.printStackTrace();
-        }
+        File off = new File(outPath + "_offset.txt");
 
         try {
-
-            BufferedWriter bw = new BufferedWriter(new FileWriter(outPath + instance + "_offset.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(outPath + "_offset.txt"));
 
             for (i = 0; i < n; i++) {
                 m = offset[i].length;
@@ -450,20 +405,18 @@ public class GroupVarInt {
                 bw.newLine();
             }
             bw.flush();
-
-
         } catch (IOException e) {
-        }
-        logger.info("Encoded Graph and offset files properly written " );
 
+        }
     }
 
     /**
      * Return the compressed adjacency list
      * @return compressed adjacency list (byte array)
      */
-    public byte[] get_compressedAdjListFlat(){
-        return(compressedAdjListFlat);
+    public byte[] get_compressedAdjListFlat() {
+        return compressedAdjListFlat;
     }
 
 }
+
